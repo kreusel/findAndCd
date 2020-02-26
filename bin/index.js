44e590bbc53f12a6789e2df69eeef26ea9466571
Michael Kreusel <michael@kreusel.info>
2020-02-26 17:00:47 +0100

    added selector for multiple matches

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,17 @@
 #!/bin/env node
+const cliSelect = require('cli-select');
+
+const innerSearch = require('../lib/findAndCd')
+
 const args = process.argv.slice(2)
 const folderToFind = args[0]
 const folderToSearch = args[1] || process.cwd()
-const innerSearch = require('../lib/findAndCd')
 
-
-const result = innerSearch(folderToFind, folderToSearch)
-if (result) console.log(result)
+const values = innerSearch(folderToFind, folderToSearch)
+if (values.length >= 2) {
+    cliSelect({values}).then(({value}) => {
+        console.log(value)
+    }).catch(() => { })
+} else if (values.length === 1) {
+    console.log(values[0])
+}
