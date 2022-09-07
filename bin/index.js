1a3b7fc927c73d8b99cee2103c57149be3f19201
Michael Kreusel <michael@kreusel.info>
2022-09-07 22:02:59 +0200

    save

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,4 @@
 #!/bin/env node
-const cliSelect = require('cli-select');
-
 const innerSearch = require('../lib/findAndCd')
 
 const args = process.argv.slice(2)
@@ -8,10 +6,5 @@ const folderToFind = args[0]
 const folderToSearch = args[1] || process.cwd()
 
 const values = innerSearch(folderToFind, folderToSearch)
-if (values.length >= 2) {
-    cliSelect({values}).then(({value}) => {
-        console.log(value)
-    }).catch(() => { })
-} else if (values.length === 1) {
-    console.log(values[0])
-}
+if (values.length != 0)
+    console.log(values.join(" "))
